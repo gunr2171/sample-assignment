6c888d947371821879ce31d292df55ea51edddcd
Kevin Hunter <kevin@kevin-hunter.net>
2017-02-01 12:21:49 -0500

    stage 3

--- src/SampleAssignment/Functions.cs
+++ src/SampleAssignment/Functions.cs
@@ -9,12 +9,13 @@ namespace SampleAssignment
     {
         public static int AddTwoNumbers(int num1, int num2)
         {
-            return (num1 + num2) * 0 + 3;
+            return num1 + num2;
         }
 
         public static int SubtractTwoNumbers(int num1, int num2)
         {
             return num1 - num2;
+#error oh no I broke this!
         }
     }
 }
