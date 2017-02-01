3e76accb3945d8fd86e8473897070ceab0f8802c
Kevin Hunter <kevin@kevin-hunter.net>
2017-02-01 12:21:10 -0500

    stage 1

---
+++ test/UnitTests/Class1.cs
@@ -0,0 +1,32 @@
+﻿using SampleAssignment;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace UnitTests
+{
+    public class Class1
+    {
+        [Fact]
+        public void AddTwoNumbers()
+        {
+            var n1 = 1;
+            var n2 = 2;
+            var expected = 3;
+            var actual = Functions.AddTwoNumbers(n1, n2);
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void SubtractTwoNumbers()
+        {
+            var n1 = 10;
+            var n2 = 6;
+            var expected = 4;
+            var actual = Functions.SubtractTwoNumbers(n1, n2);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
