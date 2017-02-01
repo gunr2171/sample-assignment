4a2210381510dfc38ad068685529b506f49e9008
Kevin Hunter <kevin@kevin-hunter.net>
2017-02-01 12:21:10 -0500

    stage 1

---
+++ src/SampleAssignment/Functions.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SampleAssignment
+{
+    public static class Functions
+    {
+        public static int AddTwoNumbers(int num1, int num2)
+        {
+            return (num1 + num2) * 0 + 3;
+        }
+
+        public static int SubtractTwoNumbers(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+    }
+}
